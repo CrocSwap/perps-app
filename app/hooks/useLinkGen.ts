d023e2480c6c464e7822af726dbed43ee4aad40e
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-04-14 11:49:27 -0400

    add location data to `useLinkGen.ts` file

--- app/hooks/useLinkGen.ts
+++ app/hooks/useLinkGen.ts
@@ -1,9 +1,13 @@
+import { useLocation } from 'react-router';
+
 const pageRoutes = {
     index: '',
     trade: '/trade',
-    vaults: '/vaults'
+    vaults: '/vaults',
 };
 
 export function useLinkGen() {
+    const loc = useLocation();
     console.log('pages: ', pageRoutes);
+    console.log(loc);
 }
