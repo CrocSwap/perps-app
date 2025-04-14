2228b6e7fb323683bb924bd08e5fe7b53ecdf334
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-04-14 11:55:20 -0400

    add logic to `useLinkGen.ts` to determine is user is on a given page

--- app/hooks/useLinkGen.ts
+++ app/hooks/useLinkGen.ts
@@ -4,10 +4,16 @@ const pageRoutes = {
     index: '',
     trade: '/trade',
     vaults: '/vaults',
+    testpage: '/testpage',
 };
 
-export function useLinkGen() {
-    const loc = useLocation();
+export type pagesT = keyof typeof pageRoutes;
+
+export function useLinkGen(p: pagesT) {
+    const { pathname } = useLocation();
     console.log('pages: ', pageRoutes);
-    console.log(loc);
+    console.log(pathname);
+
+    const isPage: boolean = pathname.toLowerCase().startsWith(pageRoutes[p].toLowerCase());
+    console.log(isPage);
 }
--- app/routes/testpage/testpage.tsx
+++ app/routes/testpage/testpage.tsx
@@ -3,7 +3,7 @@ import styles from './testpage.module.css';
 
 // main react fn
 export default function testpage() {
-    useLinkGen();
+    useLinkGen('testpage');
     return (
         <div className={styles.testpage}>
             <h2>Hi there!</h2>
