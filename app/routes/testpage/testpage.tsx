ce2c48926af6193c9289dad5be1b50c2892e75f2
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-04-14 12:55:19 -0400

    reset testpage

--- app/routes/testpage/testpage.tsx
+++ app/routes/testpage/testpage.tsx
@@ -1,16 +1,9 @@
-import { useLinkGen } from '~/hooks/useLinkGen';
 import styles from './testpage.module.css';
 
 // main react fn
 export default function testpage() {
-    const PAGE = 'vaults';
-    const { isPage } = useLinkGen(PAGE);
     return (
         <div className={styles.testpage}>
-            <h2>Hi there!</h2>
-            <p>
-                This is {isPage || 'not'} the page "{PAGE}".
-            </p>
         </div>
     );
 }
