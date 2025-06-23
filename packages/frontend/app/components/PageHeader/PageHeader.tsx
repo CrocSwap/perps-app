5c0f1cb0bdc141d9d292d23665be555d740c465d
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-06-23 15:05:16 -0400

    test update

--- packages/frontend/app/components/PageHeader/PageHeader.tsx
+++ packages/frontend/app/components/PageHeader/PageHeader.tsx
@@ -39,7 +39,7 @@ export default function PageHeader() {
     const navLinks = [
         { name: 'Trade', path: `/trade/${symbol}` },
         { name: 'Vaults', path: '/vaults' },
-        { name: 'PortfolioTest11', path: '/portfolio' },
+        { name: 'PortfolioTest12', path: '/portfolio' },
         { name: 'Referrals', path: '/referrals' },
         // { name: 'Points', path: '/points' },
         { name: 'Leaderboard', path: '/leaderboard' },
--- packages/frontend/public/sw.js
+++ packages/frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-app-cache-v0.0.12-11';
+const CACHE_NAME = 'my-app-cache-v0.0.12-12';
 const ASSETS_TO_CACHE = [
     '/',
     '/favicon.ico',
