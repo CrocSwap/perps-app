1bad7e04f501e74e825cc94c9629865b0ad6fcdf
Emily Drake Smith <emily.drake.smith@gmail.com>
2026-01-29 16:36:21 -0500

    add data and methods to `ReferralStore.ts` file

--- packages/frontend/app/stores/ReferralStore.ts
+++ packages/frontend/app/stores/ReferralStore.ts
@@ -1,10 +1,20 @@
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
+export interface RefCodeCacheIF {
+    code: string;
+    isCodeRegistered: boolean | undefined;
+    isCodeApprovedByInvitee: boolean | undefined;
+}
+
 export interface ReferralStoreIF {
     cached: string;
+    cached2: RefCodeCacheIF;
     totVolume: number | undefined;
     cache(refCode: string): void;
+    cache2(refCode: string): void;
+    markCodeRegistered(refCode: string): void;
+    markCodeApproved(refCode: string): void;
     setTotVolume(volume: number | undefined): void;
     clear(): void;
 }
@@ -25,11 +35,68 @@ const ssrSafeStorage = () =>
 
 export const useReferralStore = create<ReferralStoreIF>()(
     persist(
-        (set) => ({
+        (set, get) => ({
             cached: '',
+            cached2: {
+                code: '',
+                isCodeRegistered: undefined,
+                isCodeApprovedByInvitee: undefined,
+            },
             totVolume: undefined,
             cache(refCode: string): void {
                 set({ cached: refCode });
+            },
+            cache2(refCode: string): void {
+                set({
+                    cached2: {
+                        code: refCode,
+                        isCodeRegistered: undefined,
+                        isCodeApprovedByInvitee: undefined,
+                    },
+                });
+            },
+            markCodeRegistered(refCode: string): void {
+                const codeFromCache: string = get().cached2.code;
+                if (refCode === codeFromCache) {
+                    set({
+                        cached2: {
+                            code: codeFromCache,
+                            isCodeRegistered: true,
+                            isCodeApprovedByInvitee:
+                                get().cached2.isCodeApprovedByInvitee,
+                        },
+                    });
+                } else {
+                    const dataForError = {
+                        cached: codeFromCache,
+                        approvalReceivedFor: refCode,
+                    };
+                    console.error(
+                        'Code in LS cache does not match code queried for registration.',
+                        dataForError,
+                    );
+                }
+            },
+            markCodeApproved(refCode: string): void {
+                const codeFromCache: string = get().cached2.code;
+                if (refCode === codeFromCache) {
+                    set({
+                        cached2: {
+                            code: codeFromCache,
+                            isCodeRegistered: get().cached2.isCodeRegistered,
+                            isCodeApprovedByInvitee: true,
+                        },
+                    });
+                } else {
+                    const dataForError = {
+                        cached: codeFromCache,
+                        approvalReceivedFor: refCode,
+                    };
+                    console.error(
+                        'Code in LS cache does not match code queried for approval.',
+                        dataForError,
+                    );
+                }
             },
             setTotVolume(volume: number | undefined): void {
                 set({ totVolume: volume });
