e0678519caf5b7715288c697fa1a33432ab64c34
Emily Drake Smith <emily.drake.smith@gmail.com>
2026-04-01 14:57:00 -0400

    add type gaurding to FUUL key management

--- packages/frontend/app/components/Referrals/referralKeys.ts
+++ packages/frontend/app/components/Referrals/referralKeys.ts
@@ -1,7 +1,7 @@
 // IMPORTANT:   this is a temporary file being used for dev purposes only and should be removed before merging to main
 // IMPORTANT:   ... will be replaced with better handling before launch
 
-interface FUUL_API_KEY_SET {
+export interface FUUL_API_KEY_SET {
     // Grants read-only access to the project information, conversions and rewards
     READ_ONLY: string;
     // Grants access to send tracking events (e.g: pageview, connect_wallet)
@@ -14,7 +14,7 @@ interface FUUL_API_KEY_SET {
     REFERRAL_CODES_READ: string;
 }
 
-const NON_PERMISSIONED: FUUL_API_KEY_SET = {
+export const NON_PERMISSIONED: FUUL_API_KEY_SET = {
     READ_ONLY:
         '459f44f19dd5e3d7a8e2953fb0742ed98736abc42873b6c35c4847585c781661',
     SEND_TRACKING_EVENT: '',
@@ -25,7 +25,7 @@ const NON_PERMISSIONED: FUUL_API_KEY_SET = {
         '5208e0f9fd95f4dd7897fc383186cd2870cb45446f4c5d7fc75f670df02132fa',
 };
 
-const PERMISSIONED: FUUL_API_KEY_SET = {
+export const PERMISSIONED: FUUL_API_KEY_SET = {
     READ_ONLY:
         'fa6829880bf45b720329c5ab1b6beafad705b6cc3967981120e31d398568f4e0',
     SEND_TRACKING_EVENT: '',
@@ -35,3 +35,24 @@ const PERMISSIONED: FUUL_API_KEY_SET = {
     REFERRAL_CODES_READ:
         'f0273090097e61ce706cc8c6423b7da119bc5e13896ac30c7187cdbb339abf0a',
 };
+
+export type READ_ONLY_KEY = (
+    | typeof NON_PERMISSIONED
+    | typeof PERMISSIONED
+)['READ_ONLY'];
+export type SEND_TRACKING_EVENT_KEY = (
+    | typeof NON_PERMISSIONED
+    | typeof PERMISSIONED
+)['SEND_TRACKING_EVENT'];
+export type SEND_TRIGGER_EVENT_KEY = (
+    | typeof NON_PERMISSIONED
+    | typeof PERMISSIONED
+)['SEND_TRIGGER_EVENT'];
+export type SERVICE_ROLE_KEY = (
+    | typeof NON_PERMISSIONED
+    | typeof PERMISSIONED
+)['SERVICE_ROLE'];
+export type REFERRAL_CODES_READ_KEY = (
+    | typeof NON_PERMISSIONED
+    | typeof PERMISSIONED
+)['REFERRAL_CODES_READ'];
