e9659a17e1a352a11020010c7c7ac39d55a6e3a4
Emily Drake Smith <emily.drake.smith@gmail.com>
2026-04-02 17:12:25 -0400

    remove mock data

--- packages/frontend/app/components/Referrals/ReferralsTable/ReferralsTable.tsx
+++ packages/frontend/app/components/Referrals/ReferralsTable/ReferralsTable.tsx
@@ -2,9 +2,6 @@ import { memo } from 'react';
 import styles from './ReferralsTable.module.css';
 import ReferralsTableHeader from './ReferralsTableHeader';
 import ReferralsTableRow from './ReferralsTableRow';
-import { useReferralsTable } from './useReferralsTable';
-import { referralData } from './data';
-import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import type {
     PayoutByReferrerT,
     PayoutMovementIF,
@@ -18,29 +15,9 @@ interface PropsIF {
 function ReferralsTable(props: PropsIF) {
     const { payoutsByReferrer } = props;
 
-    const {
-        currentItems,
-        currentPage,
-        totalPages,
-        totalItems,
-
-        startIndex,
-        endIndex,
-        goToNextPage,
-        goToPreviousPage,
-        sortConfig,
-        handleSort,
-    } = useReferralsTable({
-        data: referralData,
-        itemsPerPage: 10,
-    });
-
-    const isPrevButtonDisabled = currentPage === 1;
-    const isNextButtonDisabled = currentPage === totalPages;
-
     return (
         <div className={styles.tableWrapper}>
-            <ReferralsTableHeader sortConfig={sortConfig} onSort={handleSort} />
+            <ReferralsTableHeader sortConfig={null} onSort={() => {}} />
             <div className={styles.tableBody}>
                 {payoutsByReferrer.map((referral, index) => (
                     <ReferralsTableRow
@@ -53,34 +30,6 @@ function ReferralsTable(props: PropsIF) {
                     <div className={styles.emptyState}>No data to display</div>
                 )}
             </div>
-
-            {payoutsByReferrer.length > 0 && (
-                <div className={styles.paginationContainer}>
-                    <div className={styles.pageInfo}>
-                        {totalItems > 0
-                            ? `${startIndex + 1}-${Math.min(endIndex + 1, totalItems)} of ${totalItems}`
-                            : '0-0 of 0'}
-                    </div>
-
-                    <div className={styles.pageButtons}>
-                        <button
-                            className={styles.pageButton}
-                            onClick={goToPreviousPage}
-                            disabled={isPrevButtonDisabled}
-                        >
-                            <BiChevronLeft size={16} />
-                        </button>
-
-                        <button
-                            className={styles.pageButton}
-                            onClick={goToNextPage}
-                            disabled={isNextButtonDisabled}
-                        >
-                            <BiChevronRight size={16} />
-                        </button>
-                    </div>
-                </div>
-            )}
         </div>
     );
 }
